e5c2bca9efb3f695ed87b7fc7b411d1935b816b3
Hung <69558780+nguyenhung15913@users.noreply.github.com>
2021-10-14 19:21:16 -0400

    Added a burger menu icon, when toggle, sidebar will be display

--- src/pages/Sidebar.js
+++ src/pages/Sidebar.js
@@ -1,24 +1,34 @@
-import React from 'react';
-import AppLogo from '../assets/images/logo.png';
-import {Link} from 'react-router-dom';
+import React, { useState } from "react";
+import AppLogo from "../assets/images/logo.png";
+import { Link } from "react-router-dom";
+import BurgerMenu from "../pages/BurgerMenu";
 
 export default function Sidebar() {
-    return (
-        <div className="Sidebar">
-            <div className="App-logo">
-                <img src={AppLogo} alt="app logo"/>
-            </div>
-            <div className="Menu">
-                <Link to="/">
-                    <div className="Menu-item">Tasks</div>
-                </Link>
-                <Link to="/settings">
-                    <div className="Menu-item">Settings</div>
-                </Link>
-                <Link to="/about">
-                    <div className="Menu-item">About</div>
-                </Link>
-            </div>
-        </div>
-    );
+	const [active, setActive] = useState(false);
+
+	const isSideBarActive = () => {
+		setActive(!active);
+	};
+
+	return (
+		<div>
+			<BurgerMenu active={active} toggleSidebar={isSideBarActive} />
+			<div className={active ? "Sidebar active" : "Sidebar"}>
+				<div className="App-logo">
+					<img src={AppLogo} alt="app logo" />
+				</div>
+				<div className="Menu">
+					<Link to="/">
+						<div className="Menu-item">Tasks</div>
+					</Link>
+					<Link to="/settings">
+						<div className="Menu-item">Settings</div>
+					</Link>
+					<Link to="/about">
+						<div className="Menu-item">About</div>
+					</Link>
+				</div>
+			</div>
+		</div>
+	);
 }
