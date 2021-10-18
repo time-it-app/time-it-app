ca592354e5a96aba4d5812b8617c9cde8a2e6395
itsmais <mih28@mail.aub.edu>
2021-10-19 01:26:28 +0200

    making sidebar showing on navigation

--- src/pages/Sidebar.js
+++ src/pages/Sidebar.js
@@ -4,31 +4,31 @@ import { Link } from "react-router-dom";
 import BurgerMenu from "../pages/BurgerMenu";
 
 export default function Sidebar() {
-	const [active, setActive] = useState(false);
+  const [active, setActive] = useState(true);
 
-	const isSideBarActive = () => {
-		setActive(!active);
-	};
+  const isSideBarActive = () => {
+    setActive(!active);
+  };
 
-	return (
-		<div className={active ? "Navigation active" : "Navigation"}>
-			<div className="Sidebar">
-				<div className="App-logo">
-					<img src={AppLogo} alt="app logo" />
-				</div>
-				<div className="Menu">
-					<Link to="/">
-						<div className="Menu-item">Tasks</div>
-					</Link>
-					<Link to="/settings">
-						<div className="Menu-item">Settings</div>
-					</Link>
-					<Link to="/about">
-						<div className="Menu-item">About</div>
-					</Link>
-				</div>
-			</div>
-			<BurgerMenu active={active} toggleSidebar={isSideBarActive} />
-		</div>
-	);
+  return (
+    <div className={active ? "Navigation active" : "Navigation"}>
+      <div className="Sidebar">
+        <div className="App-logo">
+          <img src={AppLogo} alt="app logo" />
+        </div>
+        <div className="Menu">
+          <Link to="/">
+            <div className="Menu-item"> Tasks </div>
+          </Link>
+          <Link to="/settings">
+            <div className="Menu-item"> Settings </div>
+          </Link>
+          <Link to="/about">
+            <div className="Menu-item"> About </div>
+          </Link>
+        </div>
+      </div>
+      <BurgerMenu active={active} toggleSidebar={isSideBarActive} />
+    </div>
+  );
 }
